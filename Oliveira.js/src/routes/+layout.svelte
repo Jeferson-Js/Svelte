dd518378eab562994fd8a4545bd50e7446206ee0
Jeferson-Js <oliveira.jeferson.jsogmail.com>
2023-04-13 17:17:35 -0300

    Refatorando a aplicação e adicionando novas tags para melhoria de seo.

--- Oliveira.js/src/routes/+layout.svelte
+++ Oliveira.js/src/routes/+layout.svelte
@@ -1,10 +1,14 @@
 <script>
   import "../app.postcss";
   import Footer from "../components/Footer.svelte";
+  import Home from "../components/Home.svelte";
   import Navbar from "../components/Navbar.svelte";
 </script>
+
 <main>
   <Navbar />
   <slot />
   <Footer />
 </main>
+
+

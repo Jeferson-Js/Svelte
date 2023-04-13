ecf2469ed92c399f4632877fe15930a49bab6a44
Jeferson-Js <oliveira.jeferson.jsogmail.com>
2023-04-13 15:11:46 -0300

    refatorando a aplicação

--- Oliveira.js/svelte.config.js
+++ Oliveira.js/svelte.config.js
@@ -1,11 +1,13 @@
 import preprocess from "svelte-preprocess";
-import adapter from "@sveltejs/adapter-netlify";
+import adapter from "@sveltejs/adapter-auto";
 
 const config = {
   kit: {
     adapter: adapter(),
+    target: '#svelte'
   },
   preprocess: preprocess(),
 };
 
 export default config;
+
